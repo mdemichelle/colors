af089fe8ed2a4308e44d9281e9b1df724d1650e5
michellemb306 <michellemirandab@gmail.com>
2020-09-25 23:14:35 -0300

    Agregué archivo json. Muestro los colores en divs y filtro desde input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,36 @@
-const app = new Vue({
+var app = new Vue({
     el: '#app',
     data: {
-      message: 'Hola vue',
-      search: '',
-   
+        message: '',
+        search: '',
+        json: {},
+        colors: [],
     },
-    methods:{
+    computed: {
 
+        filteredColors() {
+            //filtra el array colors[] según los carácteres ingresados en el input
+            return this.colors.filter(color => {
+                return color.name.toLowerCase().indexOf(this.search.toLowerCase()) > -1
+            })
+        }
     },
-}
-    )
+    mounted() {
+        //llamada a archivo colors.json
+        axios.get('./colors.json')
+            .then((response) => {
+                //nos interesa solo el contenido de data
+                this.json = response.data;
+            }).then(() => {
+                //luego de tener la información se itera la variable json y se agregan al array con etiqueta name y color
+                for (let element in this.json) {
+                    this.colors.push({
+                        name: element,
+                        color: this.json[element]
+                    });
+                }
+            })
+    }
+
+
+})
